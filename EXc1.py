7cc768d62b1d1755e393e4ad80a2257e8644e21a
OmarMahmoud94 <80782446+OmarMahmoud94@users.noreply.github.com>
2021-07-30 18:36:12 +0200

    Delete EXc1.py

--- EXc1.py
+++
@@ -1,7 +0,0 @@
-import math
-while True:
-    first_point = input("Enter first point in form of a,b :  ").split(",")
-    Second_point = input("Enter second point in form of a,b : ").split(",")
-    res = math.sqrt(pow((int(Second_point[0]) - int(first_point[0])), 2)
-                    + pow((int(Second_point[1]) - int(first_point[1])), 2))
-    print(res)
